08a403ec91dd64cdb9281aace89214525dbb2263
Eduard <mr.herocaner@gmail.com>
2024-05-18 17:32:27 +0300

    Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,4 +1,4 @@
-﻿int[,] matrix = new int [3, 5];
+﻿int[,] matrix = new int[3, 5];
 
 Random rnd = new Random();
 for (int i = 0; i < matrix.GetLength(0); i++)
@@ -13,6 +13,36 @@ for (int i = 0; i < matrix.GetLength(0); i++)
 {
     for (int j = 0; j < matrix.GetLength(1); j++)
     {
-        Console.WriteLine($"{matrix[i, j]} ");
+        Console.Write($"{matrix[i, j]} ");
+    }
+    Console.WriteLine();
+}
+
+int[,] CreateMatrix(int rowCount, int columsCount)
+{
+        int[,] matrix = new int[columsCount, rowCount];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 11);
+        }
+    }
+    return matrix;
+}
+
+void ShowMatrix(int[,] matrix)
+{
+for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
     }
 }
+
+int[,] Matrix = CreateMatrix(4, 5);
+ShowMatrix(Matrix);
